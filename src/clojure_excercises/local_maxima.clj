d4ad193200acdc156e4cfcf10ef359cbc26c5e75
tinamt <codertina@gmail.com>
2017-05-03 15:51:29 +0530

    Tina | Same solution without recursion

--- src/clojure_excercises/local_maxima.clj
+++ src/clojure_excercises/local_maxima.clj
@@ -1,11 +1,14 @@
 (ns clojure-excercises.local_maxima)
-(def localMaxima(fn [input]
-                  (cond
-                    (< (count input) 3) '()
-                    (= (count input) 3) (if (and ( > (nth input 1) (nth input 0) ) ( > (nth input 1) (nth input 2) )) (list (nth input 1))  '())
-                    :else (concat (localMaxima (subvec (vec input) 0 3)) (localMaxima (subvec (vec input) 1)))
-                    ))
+(def splitIntoThrees(fn [input]
+                  (map vector input (rest input) (rest (rest input)))
                   )
+  )
+(def isLocalMaxima (fn [triplet]
+                     (and ( > (nth triplet 1) (nth triplet 0) ) ( > (nth triplet 1) (nth triplet 2) ))
+                     ))
 
-
+(def localMaxima (fn [input]
+                   (map #(nth % 1 ) (filter (fn [triplet]
+                                               (= (isLocalMaxima triplet) true) ) (splitIntoThrees input)))
+                   ))
 
